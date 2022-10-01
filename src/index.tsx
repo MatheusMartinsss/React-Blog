ea3e0b802dbda559cbc340c0198e29d1f906b8ae
MatheusMartinsss <matheus.s.martins98@gmail.com>
2022-10-01 11:46:09 -0300

    setting react typescript

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Routers from './main/routers/Routers'
+
+ReactDOM.render(
+  <Routers />,
+  document.getElementById('main')
+)
---
+++ src/main/routers/Routers.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Routers: React.FC =() => {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default Routers;
