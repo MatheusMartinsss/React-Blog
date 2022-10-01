614b005a633d5a6f8aa364b312cca01e69936d26
MatheusMartinsss <matheus.s.martins98@gmail.com>
2022-10-01 12:34:24 -0300

    add antd style

--- src/main/routers/Routers.tsx
+++ src/main/routers/Routers.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { MakeHome } from '../factories/pages/'
-
+import './Routers.css';
 const Router: React.FC = () => {
   return (
     <BrowserRouter>
