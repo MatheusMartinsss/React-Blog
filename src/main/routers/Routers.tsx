23f27c5da07ed263f5da8ffa0f74f818eec35fa0
MatheusMartinsss <matheus.s.martins98@gmail.com>
2022-10-04 13:07:07 -0300

    remove mainlayout from routers

--- src/main/routers/Routers.tsx
+++ src/main/routers/Routers.tsx
@@ -1,17 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { MakeHome } from '../factories/pages/'
-import MainLayout from '../../presentation/components/layout/main-layout';
-import './Routers.css';
+import './index.css'
 const Router: React.FC = () => {
   return (
-    <MainLayout>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/home" element={<MakeHome />} />
-        </Routes>
-      </BrowserRouter>
-    </MainLayout>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/home" element={<MakeHome />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 export default Router;
