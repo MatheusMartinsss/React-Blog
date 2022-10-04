e7fd3b52d34fcd72614f4cfae862ac121dab610f
MatheusMartinsss <matheus.s.martins98@gmail.com>
2022-10-04 13:07:27 -0300

    create home page

--- src/presentation/pages/home/Home.tsx
+++ src/presentation/pages/home/Home.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
-
-
+import PostCard from '../../components/postcard/post-card'
+import { Grid } from '@mui/material'
+import MainLayout from '../../components/layout/main-layout'
 const Home: React.FC = () => {
     return (
-
-        <h1> Hello World</h1>
+        <MainLayout>
+            <Grid container spacing={2}>
+                <Grid item xs={12} md={12}>
+                    <PostCard
+                        tittle='Novo Post'
+                        description='Post de teste'
+                    />
+                </Grid>
+            </Grid>
+        </MainLayout>
 
     )
 }
