9510242f4785d367dcc144e0d9c0426cf7b01a6b
MatheusMartinsss <matheus.s.martins98@gmail.com>
2022-10-04 13:06:39 -0300

    create header component

--- src/presentation/components/header/header.tsx
+++ src/presentation/components/header/header.tsx
@@ -1,21 +1,33 @@
 import React from 'react'
-import { Layout, Menu, } from 'antd'
+import { Box, AppBar, Toolbar, Typography, Button, Stack, Avatar } from '@mui/material'
 import { MenuOptions } from './menuItems'
-const { Header: Heade } = Layout
-
 const Header: React.FC = () => {
+    const [logged, setLogged] = React.useState<boolean>(false)
+    const auth = () => setLogged((old) => !old)
     return (
-        <Heade style={{
-            position: 'fixed',
-            zIndex: 1,
-            width: '100%',
-        }}>
-            <Menu
-                theme='dark'
-                mode='horizontal'
-                items={MenuOptions}
-            />
-        </Heade>
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="static" color='default'>
+                <Toolbar sx={{ justifyContent: 'space-between' }}>
+                    <Stack direction='row' spacing={2} >
+                        {MenuOptions.map(({ label, key }) => (
+                            <Typography key={key} variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                                {label}
+                            </Typography>
+                        ))}
+                    </Stack>
+                    {logged ? (
+                        <Stack direction='row'>
+                            <Button onClick={auth} color="inherit">logout</Button>
+                            <Avatar />
+                        </Stack>
+                    ) : (
+                        <Stack direction='row'>
+                            <Button onClick={auth} color="inherit">Login</Button>
+                        </Stack>
+                    )}
+                </Toolbar>
+            </AppBar>
+        </Box>
     )
 }
 
